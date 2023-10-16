63ebfcf6de7c516e1fd2d13174235dd101456f87
Victor Ma <victorma649@gmail.com>
2023-10-16 00:06:35 -0400

    Use simple buttong for ko-fi

--- src/Footer.js
+++ src/Footer.js
@@ -65,18 +65,15 @@ export default function Footer() {
       <FooterAlert title={"About"} open={aboutOpen} onClose={handleAboutClose}>
         <DialogContentText>TODO</DialogContentText>
         <br />
-        <iframe
-          id="kofiframe"
-          src="https://ko-fi.com/mimid/?hidefeed=true&widget=true&embed=true&preview=true"
-          style={{
-            border: "none",
-            width: "100%",
-            padding: "4px",
-            background: "#f9f9f9",
-          }}
-          height="615px"
-          title="mimid"
-        ></iframe>
+        <a href="https://ko-fi.com/T6T8Q4BKC" target="_blank" rel="noreferrer">
+          <img
+            height="36"
+            style={{ border: "0px", height: "36px" }}
+            src="https://storage.ko-fi.com/cdn/kofi1.png?v=3"
+            border="0"
+            alt="Buy Me a Coffee at ko-fi.com"
+          />
+        </a>
       </FooterAlert>
     </footer>
   );
