22302e78cf917ed07820fb0e2998e9975ee8d9b6
Victor Ma <victorma649@gmail.com>
2023-10-18 08:40:02 -0400

    Remove unnecessary test file

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,22 +1,3 @@
-/*
-Copyright 2023 Victor Ma
-
-This file is part of Musician's Remote.
-
-Musician's Remote is free software: you can redistribute it and/or modify it
-under the terms of the GNU Affero General Public License as published by the
-Free Software Foundation, either version 3 of the License, or (at your option)
-any later version.
-
-Musician's Remote is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
-more details.
-
-You should have received a copy of the GNU Affero General Public License along
-with Musician's Remote. If not, see <https://www.gnu.org/licenses/>.
-*/
-
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
